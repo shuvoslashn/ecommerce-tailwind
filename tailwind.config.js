56579334200f6a6f3986617be4349e5a315604e0
Shuvo Sarker <sarker3664@gmail.com>
2023-08-15 01:53:39 +0600

    docs - font setup done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ['./src/*.html'],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                poppins: "'Poppins', sans-serif",
+            },
+        },
     },
     plugins: [],
 };
