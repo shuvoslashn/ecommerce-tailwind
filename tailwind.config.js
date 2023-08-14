34193e6abb362b85ef2ae336a0bf151825753d00
Shuvo Sarker <sarker3664@gmail.com>
2023-08-15 02:00:05 +0600

    docs - customizing screens and container

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
     content: ['./src/*.html'],
     theme: {
+        screens: {
+            sm: '576px',
+            md: '768px',
+            lg: '992px',
+            xl: '1200px',
+        },
+        container: {
+            center: true,
+            padding: '1rem',
+        },
         extend: {
             fontFamily: {
                 poppins: "'Poppins', sans-serif",
