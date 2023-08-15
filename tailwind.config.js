f55a09edb6372c7f5714b74777cc1f23c5cde500
Shuvo Sarker <sarker3664@gmail.com>
2023-08-15 14:21:32 +0600

    refector - updated output.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,6 @@
 module.exports = {
     content: ['./src/*.html'],
     theme: {
-        screens: {
-            sm: '576px',
-            md: '768px',
-            lg: '992px',
-            xl: '1200px',
-        },
         container: {
             center: true,
             padding: '1rem',
